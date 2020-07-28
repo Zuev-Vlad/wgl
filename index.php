673020f8c42f0d10e7f47fad1bd2ac018c22bf2d
Vlad Z <v.zui@yandex.ru>
2020-07-28 22:33:08 +0300

    Вечер 28.07.

--- index.php
+++ index.php
@@ -11,8 +11,9 @@
 <body>
 <h1>Привет!</h1>
 <canvas id="first-scene"></canvas>
-<script src="/js/three.min.js"></script>
+<script src="/js/three.js"></script>
 <script src="/js/core.js"></script>
+<script src="/js/data-gui.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
 </body>
--- js/core.js
+++ js/core.js
@@ -5,10 +5,33 @@ $(document).ready(function() {
     
     const $body = $('body');
     const canvas = document.querySelector('#first-scene');
-
+// variable
     let width = window.screen.width;
     let height = window.screen.height;
-    
+    let ball = {
+        rotation: {
+            x:0,
+            y:0,
+            Z:0
+        },
+        position:{
+            x:0,
+            y:0,
+            z:0
+        },
+    }
+
+    let gui = new dat.gui.GUI();
+
+    for (let i in ball.rotation) {
+        gui.add(ball.rotation, i).min(-0.2).max(0.2).step(0.001);
+    }
+    for (let i in ball.position) {
+        gui.add(ball.position, i).min(-0.2).max(0.2).step(0.001);
+    }
+    // gui.add(ball, 'rotationY').min(-0.2).max(0.2).step(0.001);
+    // gui.add(ball, 'rotationX').min(-0.2).max(0.2).step(0.001);
+
     resizeFullWidthElementAttr ($('#first-scene'));
     
     $(window).resize(function() {
@@ -28,8 +51,15 @@ $(document).ready(function() {
     scene.add(light);
 
     // геометрия
-    let geometry = new THREE.ParametricGeometry( THREE.ParametricGeometries.klein, 25, 25 );
-    let material = new THREE.MeshBasicMaterial({color:0x00ff00, wireframe:true});
+    let geometry = new THREE.SphereGeometry(200, 20, 20);
+    let material = new THREE.MeshBasicMaterial({color:0xffffff, vertexColors: THREE.FaceColors});
+   
+    console.log(geometry.faces.length)
+
+    for (let i=0; i < geometry.faces.length; i++) {
+         geometry.faces[i].color.setRGB(Math.random(), Math.random(), Math.random());
+    }
+
     // Материал
     let mesh = new THREE.Mesh(geometry, material);
 
@@ -42,7 +72,14 @@ $(document).ready(function() {
         $(element).attr({'width':width+'px', 'height':height+'px'});
     }
     function loop () {
-        mesh.rotation.x += Math.PI / 100;
+        
+    for (let i in ball.rotation) {
+        mesh.rotation.i += ball.rotation[i];
+    }
+    for (let i in ball.posiiton) {
+        mesh.posiiton.i += ball.position[i];
+    }
+
         render.render(scene, camera);
         requestAnimationFrame(function(){loop();});
     }
